27156ef8fb6507e478b7755ea32f019cb6addf5b
cookta2012 <cookta2012@gmail.com>
2015-11-13 20:21:38 -0600

    Add new CyberChest
    Signed-off-by: cookta2012 <cookta2012@gmail.com>

--- CyberChest_0.10.0/control.lua
+++ CyberChest_0.10.0/control.lua
@@ -3,7 +3,6 @@ require "script.cyberchest"
 require "script.gui"
 local version = 10
 local forced_reset = false
-local tickerIndex = 1
 script.on_init(function()
 if not global.cyberchests then global.cyberchests = {} end
 global.gui = new_gui()
@@ -87,8 +86,9 @@ function cyberchest_get_from_entity(entity)
 end
 
 script.on_event(defines.events.on_tick, function(event)
+	ticker()
 	--game.players[1].print(cycles)
-	if ticker(event.tick) then
+	if event.tick % 20 ~= 0 then
 		for i,player in pairs(game.players) do
 			if player.character and player.opened and player.opened.name == 'cyberchest' then
 				thingOpened = cyberchest_get_from_entity(player.opened)
@@ -106,25 +106,11 @@ script.on_event(defines.events.on_gui_click, function(event)
 	global.gui.dispatch(event.player_index, event.element.name)
 end)
 
-function ticker(tick)
-	local ticker_chests_max = 10
-	local chest
-	for cycle = 0, ticker_chests_max do
-		curCycle = cycle + tickerIndex
-		if global.cyberchests[curCycle] ~= nil then
-			chest = global.cyberchests[curCycle]
-			if chest:is_valid() then
-				chest:state_execute()
-			else	
-				chest:destroy_beacon()
-				chest = nil
-				table.remove(global.cyberchests,i)
-			end
-			tickerIndex = tickerIndex + 1
-			cycle = cycle + 1
-		else
-		tickerIndex = 1
+function ticker()
+	-- reset index if out of bounds
+	for i,v in ipairs(global.cyberchests) do
+		if not v:on_tick() then
+			table.remove(global.cyberchests, i)
 		end
 	end
-	if tick % 20 ~= 0 then return true else return false end
 end
